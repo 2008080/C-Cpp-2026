a39c4ff82c112e3e0bbdd1b7bd73ed54413b92d6
2008080 <140701491+2008080@users.noreply.github.com>
2026-09-20 18:21:42 +0800

    Update main.c
    Modified the main function to create a running letter effect for 'hello world!' and removed the original main function logic.

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include<bits/stdc++.h>
+#include<cstdlib>
+#include<windows.h>
+using namespace std;
+int main(){
+    int i=0;
+    while(i<=50) {
+         system("cls");
+         for (int j=0;j<=i;j++) printf(" ");
+         printf("hello world!\n");
+         i+=1;
+    }
 
-int main() {
-    printf("hello world!\n");
+    while(i>=0) {
+        system("cls");
+        for (int j=0;j<=i;j++) printf(" ");
+        printf("hello world!\n");
+        i-=1;
+    }
+    Sleep(10000);
+    system("pause");
     return 0;
 }
