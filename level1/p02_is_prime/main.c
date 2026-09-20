b371e850834817da1d3562e2c8ebd941739696b8
2008080 <140701491+2008080@users.noreply.github.com>
2026-09-20 18:31:33 +0800

    Update main.c

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
-
-int main() {
-    printf("hello world!\n");
-    return 0;
+#include<bits/stdc++.h>
+using namespace std;
+int a;
+bool check(int x){
+    if(x<2)return false;
+    for(int i=2;i<=x/i;i++){
+        if(x%i==0)return false;
+    }
+    return true;
+}
+int main(){
+    scanf("%d",&a);
+    if(check(a))puts("Yes");
+    else puts("No");
 }
