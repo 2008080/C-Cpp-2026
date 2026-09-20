28c93966831b13f9d3f25995644d6c399dbd18ed
2008080 <140701491+2008080@users.noreply.github.com>
2026-09-20 18:39:23 +0800

    Update main.c

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
-
-int main() {
-    printf("hello world!\n");
-    return 0;
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+int n;
+bool st[1010];
+vector<int>primes;
+DWORD start,yend;
+int main(){
+    start=GetTickCount();
+    n=1000;
+    for(int i=2;i<=n;i++){
+        if(!st[i]) primes.push_back(i);
+        for(auto prime : primes ){
+            if(prime>n/i) break;
+            st[prime*i]=true;
+            if(i%prime==0) break;
+        }
+    }
+    for (auto i:primes) printf("%d ",i);
+    yend=GetTickCount();
+    printf("\n%dms",yend-start);
 }
