a99ed36b0d4bfc686d14140f38864c2a52faa0bd
2008080 <140701491+2008080@users.noreply.github.com>
2026-09-20 18:29:02 +0800

    Update main.c

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -3,21 +3,23 @@
 #include<windows.h>
 using namespace std;
 int main(){
-    int i=0;
-    while(i<=50) {
-         system("cls");
-         for (int j=0;j<=i;j++) printf(" ");
-         printf("hello world!\n");
-         i+=1;
+    while (1) {
+        int i=0;
+        while(i<=50) {
+            system("cls");
+            for (int j=0;j<=i;j++) printf(" ");
+            printf("hello world!\n");
+            i+=1;
+            Sleep(10);
+        }
+        while(i>=0) {
+            system("cls");
+            for (int j=0;j<=i;j++) printf(" ");
+            printf("hello world!\n");
+            i-=1;
+            Sleep(10);
+        }
     }
-
-    while(i>=0) {
-        system("cls");
-        for (int j=0;j<=i;j++) printf(" ");
-        printf("hello world!\n");
-        i-=1;
-    }
-    Sleep(10000);
-    system("pause");
+    //system("pause");
     return 0;
 }
