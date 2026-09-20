5a1796169cc9d5d08e03f97d3ef39a84e575921c
2008080 <140701491+2008080@users.noreply.github.com>
2026-09-20 18:53:08 +0800

    Update main.c

--- level1/p04_hanoi/main.c
+++ level1/p04_hanoi/main.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+void solve(int n,char a,char b,char c) {
+    if (n==1) {
+        printf("%c -> %c\n",a,c);
+        return ;
+    }
+    solve(n-1,a,c,b);
+    printf("%c -> %c\n",a,c);
+    solve(n-1,b,a,c);
+}
 
-int main() {
-    printf("hello world!\n");
-    return 0;
+int main(){
+    char a='A',b='B',c='C';
+    solve(64,a,b,c);
 }
